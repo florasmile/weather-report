9caee5e76de06a5b69683ff3a5a5ab3fbc95e974
FloritaZhao <feifeilan@hotmail.com>
2025-06-02 11:39:26 -0700

    Co-authored-by: Crystal <florasmile@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,40 @@
 "use strict";
+// define state objects and add variables and their states
+const state = {
+  increaseTempControl: null,
+  decreaseTempControl: null,
+  tempValueLable: null,
+}
+
+// action: define how to want to handle events
+const increaseTempByOne = () => {
+  const value = parseInt(state.tempValueLable.textContent);
+  value++;
+  state.tempValueLable.textContent = value.toString(); 
+  
+
+}
+// registerEvents, link the action to the element to change the state
+//when clicked on the increaseTempControl (up arrow), we want to increase the tempValue by one 
+const registerEvents = () => {
+  state.increaseTempControl.addEventListener('click', increaseTempByOne);
+}
+
+const onLoaded = () => {
+  loadControls();
+  // setRandomDog();
+  registerEvents();
+}
+// select HTML elements from the DOM
+const loadControls = () => {
+  state.increaseTempControl = document.getElementById("increaseTempControl");
+  state.decreaseTempControl = document.getElementById("decreaseTempControl");
+  state.tempValueLable = document.getElementById("tempValue");
+}
 
 
 
 
 
 
-
-
-
-
-
-
-<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+//<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
