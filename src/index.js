e3d3c77ec3177e230e3afda2b0d24ccc0290b780
FloritaZhao <feifeilan@hotmail.com>
2025-06-02 20:43:27 -0700

    wave 02 - add temp still going on

--- src/index.js
+++ src/index.js
@@ -3,37 +3,73 @@
 const state = {
   increaseTempControl: null,
   decreaseTempControl: null,
-  tempValueLable: null,
-}
+  tempValue: null,
+  temp: 60,
+  landscape: null,
+};
+
+
+const updateVisuals = () => {
+  const value = state.temp;
+  state.tempValue.textContent = value;
+  state.tempValue.className = "";
+
+  if (value >= 80) {
+    state.tempValue.classList.add("red");
+    state.landscape.textContent = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+  } else if (value >= 70) {
+    state.tempValue.classList.add("orange");
+    state.landscape.textContent = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+  } else if (value >= 60) {
+    state.tempValue.classList.add("yellow");
+    state.landscape.textContent = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+  } else if (value >= 50) {
+    state.tempValue.classList.add("green");
+    state.landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+  } else {
+    state.tempValue.classList.add("teal");
+    state.landscape.textContent = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+  }
+};
+
 
 // action: define how to want to handle events
 const increaseTempByOne = () => {
-  const value = parseInt(state.tempValueLable.textContent);
-  value++;
-  state.tempValueLable.textContent = value.toString(); 
-  
+  state.temp += 1;
+  updateVisuals();
+};
 
-}
+const decreaseTempByOne = () => {
+  state.temp -= 1;
+  updateVisuals();
+};
+
+
 // registerEvents, link the action to the element to change the state
 //when clicked on the increaseTempControl (up arrow), we want to increase the tempValue by one 
 const registerEvents = () => {
   state.increaseTempControl.addEventListener('click', increaseTempByOne);
-}
+  state.decreaseTempControl.addEventListener("click", decreaseTempByOne);
+};
 
-const onLoaded = () => {
-  loadControls();
-  // setRandomDog();
-  registerEvents();
-}
+
 // select HTML elements from the DOM
 const loadControls = () => {
   state.increaseTempControl = document.getElementById("increaseTempControl");
   state.decreaseTempControl = document.getElementById("decreaseTempControl");
-  state.tempValueLable = document.getElementById("tempValue");
-}
+  state.tempValue = document.getElementById("tempValue");
+  state.landscape = document.getElementById("landscape");
+};
 
 
+const onLoaded = () => {
+  loadControls();
+  registerEvents();
+  updateVisuals();
+};
 
+
+document.addEventListener("DOMContentLoaded", onLoaded);
 
 
 
